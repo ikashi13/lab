f7758c4425feb50c2dfac2ce9b16dfa4f3f89931
ikashi13 <66717279+ikashi13@users.noreply.github.com>
2020-06-10 11:01:21 +0300

    ()
    ()

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var matan=5;
+var bespoleznyi1=5;
+var ciscoAcademy=5;
+var ocenka=(matan+bespoleznyi1+ciscoAcademy)/3;
+
+function sayHello(name) {
+  var myName=name;
+  var message="Привет, мир! Меня зовут "+myName;
+  return message;
+}
+console.log(sayHello("Рома"));
